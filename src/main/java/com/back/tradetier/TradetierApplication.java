048953f33728cfc323f2e10516b4d0efcaf4d7d2
Juan Jose Gomez <jjgcrascafria@gmail.com>
2025-04-19 03:34:59 +0200

    Se crea la arquitectura inicial para el backend del proyecto

---
+++ src/main/java/com/back/tradetier/TradetierApplication.java
@@ -0,0 +1,13 @@
+package com.back.tradetier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradetierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradetierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/back/tradetier/TradetierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.tradetier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradetierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
