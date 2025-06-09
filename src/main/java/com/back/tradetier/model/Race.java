4b1a78482984e785eac105e87612203ce5bda8c2
Juan Jose Gomez <jjgcrascafria@gmail.com>
2025-06-09 17:33:07 +0200

    Cambio raza a no unique

--- src/main/java/com/back/tradetier/model/Race.java
+++ src/main/java/com/back/tradetier/model/Race.java
@@ -25,7 +25,7 @@ public class Race {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "name", nullable = false, unique = true)
+    @Column(name = "name", nullable = false)
     private String name;
 
 
